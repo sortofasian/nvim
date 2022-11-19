2b003d5fdfc408b81b6fdbfdd99caaa4249686c1
sortofasian <charliesyvertsen06@icloud.com>
2022-11-18 22:32:40 -0500

    Update telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,13 +9,39 @@ local builtin = require('telescope.builtin')
 local fb = telescope.extensions.file_browser
 local map = vim.keymap.set
 
+
 telescope.setup({
     defaults = {
         mappings = {
-            ['i'] = {['<C-c>'] = false, ['<CR>'] = function() vim.cmd('stopinsert') end},
-            ['n'] = {["q"] = actions.close, ['s'] = actions.toggle_selection}
+            ['i'] = {
+                ['<C-c>'] = false,
+                ['<CR>'] = function() vim.cmd('stopinsert') end
+            },
+            ['n'] = {
+                ['q'] = actions.close,
+                ['s'] = actions.toggle_selection,
+                ['H<CR>'] = actions.select_horizontal,
+                ['V<CR>'] = actions.select_vertical
+            }
         },
         initial_mode = 'normal'
+    },
+    pickers = {
+        buffers = {
+            mappings = {
+                n = {
+                    ["x"] = actions.delete_buffer
+                }
+            }
+        },
+        git_status = {
+            mappings = {
+                n = {
+                    ['<CR>'] = actions.git_staging_toggle,
+                    ['o'] = actions.select_default
+                }
+            }
+        }
     },
     extensions = {
         fzf = {
@@ -25,37 +51,34 @@ telescope.setup({
             case_mode = "smart_case"
         },
         file_browser = {
-            hijack_netrw = false,
+            grouped = true,
+            respect_gitignore = false,
+            hijack_netrw = true,
+            display_stat = {size = true},
             mappings = {
                 ["n"] = {
-                    ['<Leader>f'] = actions.close,
                     ['c'] = fb.actions.create,
                     ['d'] = fb.actions.copy,
                     ['m'] = fb.actions.move,
                     ['r'] = fb.actions.rename,
                     ['x'] = fb.actions.remove,
                     ['h'] = fb.actions.toggle_hidden,
-                    ['b'] = fb.actions.goto_parent_dir,
-                    ['<Leader>cd'] = fb.actions.change_cwd
+                    ['e'] = fb.actions.change_cwd
                 }
             }
         }
     }
 })
+
 telescope.load_extension "fzf"
 telescope.load_extension "file_browser"
 
-map('n', '<Leader>ef', builtin.diagnostics)
+map('n', '<Leader>e', builtin.diagnostics)
+map('n', '<Leader>b', builtin.buffers)
+
 map('n', '<Leader>gs', builtin.git_status)
 map('n', '<Leader>gc', builtin.git_commits)
 map('n', '<Leader>gb', builtin.git_branches)
 map('n', '<Leader>gh', builtin.git_stash)
-map('n', '<Leader>e', function() builtin.diagnostics() end)
-map('n', '<Leader>f', function()
-    fb.file_browser({
-        grouped = true,
-        path = '%:p:h',
-        cwd = vim.fn.expand('%:p:h'),
-        respect_gitignore = false
-    })
-end)
+
+map('n', '<Leader>f', fb.file_browser)
